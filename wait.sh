870450fd62d96e10dfa06f3e7b7dd41fc63896dc
Darkside0908 <mghani0908@gmail.com>
2026-07-13 09:40:10 +0700

    fix wait selenium

--- wait.sh
+++ wait.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting Selenium..."
 
-until curl -s http://localhost:4444/wd/hub/status | grep -q "\"ready\":true"
+until curl --silent http://localhost:4444/status | grep -q '"ready":true'
 do
     sleep 2
 done
